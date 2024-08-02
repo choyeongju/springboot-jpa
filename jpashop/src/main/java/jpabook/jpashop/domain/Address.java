19cb6479cf29eb94d1917f81e587d88883cd742e
choyeongju <jjudy1301@naver.com>
2024-08-02 23:36:27 +0900

    :sparkles: [feat] 엔티티 클래스 개발2

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
-@Getter
-@Setter
+@Getter //값 타입은 불변 -> setter제공 X
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
