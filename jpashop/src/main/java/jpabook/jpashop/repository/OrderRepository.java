03107212065af23ae37bef4182af91568e01397b
choyeongju <jjudy1301@naver.com>
2024-08-06 21:30:19 +0900

    :sparkles: [feat] 주문 검색 기능 구현

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,14 +17,35 @@ import java.util.List;
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
 
     public void save(Order order) {
         em.persist(order);
     }
-
-    public Order findOne(Long id){
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    @Transactional
+    public List<Order> findAll(OrderSearch orderSearch) {
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 동적 쿼리 조건 추가
+        if (orderSearch.getOrderStatus() != null) {
+            builder.and(order.status.eq(orderSearch.getOrderStatus()));
+        }
+        if (orderSearch.getMemberName() != null && !orderSearch.getMemberName().isEmpty()) {
+            builder.and(member.name.like("%" + orderSearch.getMemberName() + "%"));
+        }
+
+        return jpaQueryFactory
+                .selectFrom(order)
+                .join(order.member, member)
+                .where(builder)
+                .limit(1000)
+                .fetch();
+    }
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch { //검색 조건 파라미터
+
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus; // 주문 상태 [ORDER, CANCEL]
+}
