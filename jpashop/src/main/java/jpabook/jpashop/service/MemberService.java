cbecd5d835ba46b2cd52c94ad7d18b8dc1d7efa5
choyeongju <jjudy1301@naver.com>
2024-11-12 01:54:27 +0900

    :sparkles: [feat] 회원 수정 API

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -40,7 +40,13 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    private Member findOne(Long memberId){
+    public Member findOne(Long memberId){
         return memberRepository.findOne(memberId);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 }
